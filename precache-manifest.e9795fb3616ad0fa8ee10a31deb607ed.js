559327b0b9b36e67b71f18c2b4a829cd0a11499d
Miguel Angel <angelarkangel91@hotmail.com>
2021-06-08 15:12:55 -0500

    Updates

---
+++ precache-manifest.e9795fb3616ad0fa8ee10a31deb607ed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35407b338bc496904b267f7368288f62",
+    "url": "/games/index.html"
+  },
+  {
+    "revision": "94764990b2caaa086ec7",
+    "url": "/games/static/css/main.e2dd1764.chunk.css"
+  },
+  {
+    "revision": "ae36c0652a2b77e0d952",
+    "url": "/games/static/js/2.9f9eacb9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/games/static/js/2.9f9eacb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94764990b2caaa086ec7",
+    "url": "/games/static/js/main.8a373b14.chunk.js"
+  },
+  {
+    "revision": "06beadf2df41b0b07535",
+    "url": "/games/static/js/runtime-main.413ab4d9.js"
+  },
+  {
+    "revision": "fbcd0cf9f2e59b1d4b967d830a2bb862",
+    "url": "/games/static/media/cat.fbcd0cf9.png"
+  },
+  {
+    "revision": "b73bfb2d51e0ae30c3a6f04666c22b3e",
+    "url": "/games/static/media/facts.b73bfb2d.png"
+  },
+  {
+    "revision": "b7fad84e4f3cbdb4decfb0c5c426082d",
+    "url": "/games/static/media/paint.b7fad84e.png"
+  },
+  {
+    "revision": "0b4a5ff5454fcefbc00f529913feb844",
+    "url": "/games/static/media/suerte.0b4a5ff5.jpg"
+  }
+]);
